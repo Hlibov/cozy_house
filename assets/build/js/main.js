372e0d56d441e1a56748fab66bd354d133e9cb06
Hlibov <49684027+Hlibov@users.noreply.github.com>
2021-02-04 20:20:33 +0200

    Add files via upload

---
+++ assets/build/js/main.js
@@ -0,0 +1,35 @@
+// Импортируем другие js-файлы
+jQuery(function () {
+    initSlickCarousel();
+});
+
+// slick init
+function initSlickCarousel() {
+    jQuery('.animals-slider').slick({
+        slidesToScroll: 1,
+        rows: 0,
+        slidesToShow: 3,
+        prevArrow: '<button class="slick-prev"><span class="icon-long-left-arrow"></span></button>',
+        nextArrow: '<button class="slick-next"><span class="icon-long-left-arrow"></span></button>',
+        centerMode: true,
+        centerPadding: '0px',
+        draggable: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }, {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+}
